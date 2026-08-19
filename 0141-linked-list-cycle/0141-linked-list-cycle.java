b521257106c0f1692601077070b4053e19051c99
Nischala G.S <139974840+nischala755@users.noreply.github.com>
2026-08-19 10:41:48 +0530

    Time: 0 ms (100%), Space: 46.5 MB (87.82%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -17,10 +17,11 @@ public class Solution {
         {
             slow=slow.next;
             fast=fast.next.next;
-            if(slow==fast)
-            return true;
+            if(slow==fast) return true;
+
         }
         return false;
+        
         
     }
 }
