ee93892cc695d3195aaf39bc523d5a568fb5b6f3
Nischala G.S <139974840+nischala755@users.noreply.github.com>
2026-08-21 18:53:42 +0530

    Time: 4 ms (36.61%), Space: 43.5 MB (25.79%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
         HashMap<Character,Character> map=new HashMap<>();
-        int n=s.length();
+        Stack<Character> stack=new Stack<>();
         map.put(')','(');
         map.put('}','{');
         map.put(']','[');
-        Stack<Character> stack=new Stack<>();
-        for(int i=0;i<n;i++)
+        for(int i=0;i<s.length();i++)
         {
-            Character cur=s.charAt(i);
+            char cur=s.charAt(i);
             if(map.containsKey(cur))
             {
-                if(stack.isEmpty() || stack.pop()!=map.get(cur))
-                {
-                    return false;
-                }
-                
+                if(stack.isEmpty() || stack.pop() != map.get(cur)) return false;
             }
             else
-                stack.push(cur);
+            stack.push(cur);
         }
         return stack.isEmpty();
         
