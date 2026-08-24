86245ef28c81807b02d7361ffda61de1224b937c
Nischala G.S <139974840+nischala755@users.noreply.github.com>
2026-08-24 22:49:42 +0530

    Time: 5 ms (83.14%), Space: 77.3 MB (69.18%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,18 +3,22 @@ class Solution {
         int n=height.length;
         int left=0;
         int right=n-1;
-        int curArea=0;
-        int maxArea=0;
-        while(left<right)
+        //2 pointer approach
+        int cur_area=0;
+        int max_area=0;
+        int width=0;
+        while(left<=right)
         {
-            int heights=Math.min(height[left],height[right]);
-            int width=right-left;
-            curArea=heights*width;
-            maxArea=Math.max(curArea,maxArea);
+            int heights=Math.min(height[left],height[right]);//choose the minimum height represented by the 2 pointers
+            width=right-left;
+            cur_area=heights*width;
+            max_area=Math.max(max_area,cur_area);
             if(height[left]<height[right]) left++;
             else right--;
+            //move the 2 pointers
+
         }
-        return maxArea;
+        return max_area;//return the maximum area
         
     }
 }
