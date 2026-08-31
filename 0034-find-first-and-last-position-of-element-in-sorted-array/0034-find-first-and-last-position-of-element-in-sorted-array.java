708675aa21a1868dbec5a3936b6488243ddd8f49
Nischala G.S <139974840+nischala755@users.noreply.github.com>
2026-08-31 11:17:14 +0530

    Time: 0 ms (100%), Space: 47.9 MB (87.71%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int n=nums.length;
-        int[] res=new int[2];
+        int res[]=new int[2];
         res[0]=findFirst(nums,target);
         res[1]=findLast(nums,target);
         return res;
@@ -9,43 +9,40 @@ class Solution {
     }
     public int findFirst(int[] nums,int target)
     {
-        int start=0;
-        int end=nums.length-1;
         int idx=-1;
-        while(start<=end)
+        int n=nums.length;
+        int left=0;
+        int right=n-1;
+        while(left<=right)
         {
-            int mid=start+((end-start))/2;
-            if(nums[mid]==target) 
+            int mid=left+((right-left))/2;
+            if(nums[mid]==target)
             {
                 idx=mid;
-                end=mid-1;
-
+                right=mid-1;
             }
-            else if(nums[mid]>target) end=mid-1;
-            else start=mid+1;
+            else if(nums[mid]<target) left=mid+1;
+            else right=mid-1;
         }
         return idx;
-        
     }
     public int findLast(int[] nums,int target)
     {
-        int start=0;
-        int end=nums.length-1;
         int idx=-1;
-        while(start<=end)
+        int n=nums.length;
+        int left=0;
+        int right=n-1;
+        while(left<=right)
         {
-            int mid=start+((end-start))/2;
-            if(nums[mid]==target) 
+            int mid=left+((right-left))/2;
+            if(nums[mid]==target)
             {
                 idx=mid;
-                start=mid+1;
-
+                left=mid+1;
             }
-            else if(nums[mid]>target) end=mid-1;
-            else start=mid+1;
+            else if(nums[mid]<target) left=mid+1;
+            else right=mid-1;
         }
         return idx;
-        
     }
-
 }
